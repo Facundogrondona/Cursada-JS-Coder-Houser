3b064609d0d5d34f07766351973dfd97772c373e
Facundo Grondona <facundogrondona@gmail.com>
2022-04-09 23:41:04 -0300

    Dom - Enventos - localS - selector de nivel - liberias -

--- desafioDomEventosGrondonaFacundo/js/mains.js
+++ desafioDomEventosGrondonaFacundo/js/mains.js
@@ -7,13 +7,59 @@ let x = 0;
 let vidas = 3;
 let puntos = 0;
 
+let juagadores = [{
+	id: 1,
+	nombre: "Facundo",
+	Nivel:"Nivel 2",
+	puntos: 1500,
+	
+},
+{
+	id: 2,
+	nombre: "Mateo",
+	Nivel:"Nivel 1",
+	puntos: 500,
+	
+},
+{
+	id: 3,
+	nombre: "Tobias",
+	Nivel:"Nivel 1",
+	puntos: 700,
+	
+}
+
+];
+console.table(juagadores);
+localStorage.clear()
+
+sessionStorage.setItem('bienvenida', '¡Lautaro!');
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor)};
+
+//Almacenar producto por producto
+for (const lista of juagadores) {
+    guardarLocal(lista.id, JSON.stringify(lista));
+}
+
+guardarLocal("lista", JSON.stringify(juagadores));
+
+
+
+for (let i = 0; i < localStorage.length; i++) {
+    let clave = localStorage.key(i);
+    console.log("Clave: "+ clave);
+    console.log("Valor: "+ localStorage.getItem(clave));
+}
+
+
 // alert("Bienvenido!!!! Estas perparado para Jugar?");
 
 
 let pantalla = document.querySelector("canvas");
 let pincel = pantalla.getContext("2d");
 pincel.fillStyle = "lightgrey";
-pincel.fillRect(0, 0, 600, 400);
+pincel.fillRect(0, 0, 600, 600);
 
 
 function disenharCircunferencia(x, y, radio, color) {
@@ -25,7 +71,7 @@ function disenharCircunferencia(x, y, radio, color) {
 
 function limpiarPantalla() {
 
-	pincel.clearRect(0, 0, 600, 400);
+	pincel.clearRect(0, 0, 600, 600);
 }
 
 function disenharObjetivo(x, y, x1, y1) {
@@ -48,16 +94,37 @@ function actualizarPantalla() {
 
 	limpiarPantalla();
 	xAleatorio = sortearPosicion(600);
-	yAleatorio = sortearPosicion(400);
+	yAleatorio = sortearPosicion(600);
 	disenharObjetivo(xAleatorio, yAleatorio);
 	x++;
 	xAleatorio1 = sortearPosicion(600);
-	yAleatorio1 = sortearPosicion(400);
+	yAleatorio1 = sortearPosicion(600);
 	disenharObjetivo(xAleatorio1, yAleatorio1);
 	x++;
 }
 
-setInterval(actualizarPantalla, 1000);
+let eleccion = 1000;
+console.log(eleccion);
+
+let nivel = document.querySelector("#nivilElegido");
+nivel.addEventListener("change", ()=>{
+	let nivel = document.querySelector("#nivilElegido").value;
+   if (nivel == 1) {
+	   eleccion = 1000;
+	   console.log(eleccion)
+   }
+   if (nivel == 2) {
+	   eleccion = 2000;
+	   console.log(eleccion)
+   }
+   if (nivel == 3) {
+	   eleccion = 3000;
+	   console.log(eleccion)
+   }
+}); 
+
+
+setInterval(actualizarPantalla, eleccion);
 
 function disparar(evento) {
 
@@ -89,6 +156,11 @@ function disparar(evento) {
 		vidas = vidas - 1;
 		document.querySelector(`#vS`).innerHTML = vidas;
 		console.log(`Te quedan ${vidas} intentos`);
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: `Tienes ${vidas} de vida`,
+		  });
 
 
 	}
@@ -104,6 +176,13 @@ jugarotravez.onclick = () => {
 	document.querySelector(`#pts`).innerHTML = puntos;
 	document.querySelector(`#vS`).innerHTML = vidas;
 	console.log(`Vuelves a tener ${vidas} intentos`);
+	Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: `Vuelves a tener ${vidas} intentos`,
+        showConfirmButton: false,
+        timer: 2500
+      });
 }
 	
 	
